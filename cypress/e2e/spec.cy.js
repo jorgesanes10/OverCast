0e1f884a094936c1c97b1b876119093105f33bba
Jorge Sanes <jorgesanes10@gmail.com>
2025-01-27 10:32:19 -0500

    Fix e2e tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,12 +3,14 @@ describe('main functionality', () => {
     cy.visit('http://localhost:5173');
 
     // Search for Cartagena and add to favorites
-    cy.get('[data-testid="search-field"]').type('Cartagena{enter}');
+    cy.get('[data-testid="search-field"]').type('Cartagena');
+    cy.get('[data-testid="cities-result-0"]').click();
     cy.get('[data-testid="title"]').should('have.text', 'Cartagena (CO) ');
     cy.get('[data-testid="favorite-button"]').click();
 
     // Search for London and add to favorites
-    cy.get('[data-testid="search-field"]').clear().type('London{enter}');
+    cy.get('[data-testid="search-field"]').clear().type('London');
+    cy.get('[data-testid="cities-result-0"]').click();
     cy.get('[data-testid="title"]').should('have.text', 'London (GB) ');
     cy.get('[data-testid="favorite-button"]').click();
 
@@ -16,13 +18,8 @@ describe('main functionality', () => {
     cy.get('[data-testid="favorites-link"]').click();
 
     // Click on Cartagena (It must exist to be clicked)
-    cy.get('[data-testid="favorite-link-Cartagena"]').click();
+    cy.get('[data-testid="favorite-item-Cartagena"]').click();
     cy.get('[data-testid="title"]').should('have.text', 'Cartagena (CO) ');
-
-    // Clicks London from the search history list
-    cy.get('[data-testid="search-field"]').click();
-    cy.get('[data-testid="history-item-London"]').click();
-    cy.get('[data-testid="title"]').should('have.text', 'London (GB) ');
 
     // Removes London from the "favorites" list
     cy.get('[data-testid="favorite-button"]').click();
@@ -41,19 +38,17 @@ describe('main functionality', () => {
     );
 
     // Search for city and asserts that the URL should include "imperial"
-    cy.get('[data-testid="search-field"]').type('Cartagena{enter}');
+    cy.get('[data-testid="search-field"]').type('Cartagena');
+    cy.get('[data-testid="cities-result-0"]').click();
     cy.wait('@getWeatherData').its('request.url').should('include', 'imperial');
-
-    // Changes to metric
-    cy.get('[data-testid="units-button"]').click();
 
     // Creates a new interception for the weather API URL
     cy.intercept('https://api.openweathermap.org/data/2.5/weather*').as(
       'getWeatherData2',
     );
 
-    // Search for city and asserts that the URL should include "metric"
-    cy.get('[data-testid="search-field"]').type('{enter}');
+    // Changes to metric
+    cy.get('[data-testid="units-button"]').click();
     cy.wait('@getWeatherData2').its('request.url').should('include', 'metric');
   });
 });
